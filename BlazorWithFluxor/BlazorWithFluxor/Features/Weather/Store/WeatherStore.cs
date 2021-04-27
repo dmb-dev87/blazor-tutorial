ff180849e1b54b7f0c90282328a9b31999b4db53
dmb-dev87 <dmbdev800@gmail.com>
2021-04-27 12:08:34 -0400

    [TopOneExpert][2021-04-27][add Fluxor effect sample]

---
+++ BlazorWithFluxor/BlazorWithFluxor/Features/Weather/Store/WeatherStore.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fluxor;
+using BlazorWithFluxor.Common;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace BlazorWithFluxor.Features.Weather.Store
+{
+    public record WeatherState
+    {
+        public bool Initialized { get; init; }
+        public bool Loading { get; init; }
+        public WeatherForecast[] Forecasts { get; init; }
+    }
+
+    public class WeatherFeature : Feature<WeatherState>
+    {
+        public override string GetName()
+        {
+            return "Weather";
+        }
+
+        protected override WeatherState GetInitialState()
+        {
+            return new WeatherState
+            {
+                Initialized = false,
+                Loading = false,
+                Forecasts = Array.Empty<WeatherForecast>()
+            };
+        }
+    }
+
+    public class WeatherLoadForecastsAction { }
+
+    public class WeatherSetInitialzedAction { }
+
+    public class WeatherSetForecastsAction
+    {
+        public WeatherForecast[] Forecasts { get; }
+
+        public WeatherSetForecastsAction(WeatherForecast[] forecasts)
+        {
+            Forecasts = forecasts;
+        }
+    }
+
+    public class WeatherSetLoadingAction
+    {
+        public bool Loading { get; }
+
+        public WeatherSetLoadingAction(bool loading)
+        {
+            Loading = loading;
+        }
+    }
+
+    public static class WeatherReducers
+    {
+        [ReducerMethod]
+        public static WeatherState OnSetForecast(WeatherState state, WeatherSetForecastsAction action)
+        {
+            return state with
+            {
+                Forecasts = action.Forecasts
+            };
+        }
+
+        [ReducerMethod]
+        public static WeatherState OnSetLoading(WeatherState state, WeatherSetLoadingAction action)
+        {
+            return state with
+            {
+                Loading = action.Loading
+            };
+        }
+
+        [ReducerMethod(typeof(WeatherSetInitialzedAction))]
+        public static WeatherState OnSetInitialized(WeatherState state)
+        {
+            return state with
+            {
+                Initialized = true
+            };
+        }
+    }
+
+    public class WeatherEffects
+    {
+        private readonly HttpClient _http;
+
+        public WeatherEffects(HttpClient http)
+        {
+            _http = http;
+        }
+
+        [EffectMethod(typeof(WeatherLoadForecastsAction))]
+        public async Task LoadForecasts(IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch(new WeatherSetLoadingAction(true));
+            var forecasts = await _http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+            dispatcher.Dispatch(new WeatherSetForecastsAction(forecasts));
+            dispatcher.Dispatch(new WeatherSetLoadingAction(false));
+        }
+    }
+}
