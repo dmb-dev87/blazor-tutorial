7a71abd214deea07f95c61cb3c8ebd3c52bfb7ba
dmb-dev87 <dmbdev800@gmail.com>
2021-05-16 19:55:51 -0400

    [TopOneExpert][2021-05-16][ToastNotifications Project]

---
+++ ToastNotifications/ToastNotifications/Services/ToastService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ToastNotifications.Services
+{
+    public class ToastService : IDisposable
+    {
+        public event Action<string, ToastLevel> OnShow;
+        public event Action OnHide;
+        private Timer CountDown;
+
+        public void Dispose()
+        {
+            CountDown?.Dispose();
+        }
+
+        public void ShowToast(string message, ToastLevel level)
+        {
+            OnShow?.Invoke(message, level);
+            StartCountDown();
+        }
+
+        private void StartCountDown()
+        {
+            SetCountDown();
+
+            if (CountDown.Enabled)
+            {
+                CountDown.Stop();
+                CountDown.Start();
+            }
+            else
+            {
+                CountDown.Start();
+            }    
+        }
+
+        private void SetCountDown()
+        {
+            if (CountDown == null)
+            {
+                CountDown = new Timer(5000);
+                CountDown.Elapsed += HideToast;
+                CountDown.AutoReset = false;
+            }
+        }
+
+        private void HideToast(object source, ElapsedEventArgs args)
+        {
+            OnHide?.Invoke();
+        }
+    }
+}
