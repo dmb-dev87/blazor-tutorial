91504263f80799e8844086963d7e16a8b3ae43ec
dmb-dev87 <dmbdev800@gmail.com>
2021-05-17 12:21:47 -0400

    [TopOneExpert][2021-05-17][BlazorServerWithFluxor]

---
+++ BlazorServerWithFluxor/BlazorServerWithFluxor/Features/Counter/Store/CounterStore.cs
@@ -0,0 +1,40 @@
+﻿using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerWithFluxor.Features.Counter.Store
+{
+    public record CounterState
+    {
+        public int CurrentCounte { get; init; }
+    }
+
+    public class CounterFeature : Feature<CounterState>
+    {
+        public override string GetName() => "Counter";
+
+        protected override CounterState GetInitialState()
+        {
+            return new CounterState
+            {
+                CurrentCounte = 0
+            };
+        }
+    }
+
+    public class CounterIncrementAction { }
+
+    public static class CounterReducers
+    {
+        [ReducerMethod(typeof(CounterIncrementAction))]
+        public static CounterState OnIncrement(CounterState state)
+        {
+            return state with
+            {
+                CurrentCounte = state.CurrentCounte + 1
+            };
+        }
+    }
+}
---
+++ BlazorServerWithFluxor/BlazorServerWithFluxor/Features/Weather/Store/WeatherStore.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fluxor;
+using BlazorServerWithFluxor.Data;
+
+namespace BlazorServerWithFluxor.Features.Weather.Store
+{
+    public record WeatherState
+    {
+        public bool Initialized { get; init; }
+        public bool Loading { get; init; }
+        public WeatherForecast[] Forecasts { get; init; }
+    }
+
+    public class WeatherFeature : Feature<WeatherState>
+    {
+        public override string GetName() => "Weather";
+
+        protected override WeatherState GetInitialState()
+        {
+            return new WeatherState
+            {
+                Initialized = false,
+                Loading = false,
+                Forecasts = Array.Empty<WeatherForecast>()
+            };
+        }
+    }
+
+    
+
+    
+
+    public static class WeatherReducers
+    {
+        [ReducerMethod]
+        public static WeatherState OnSetForecasts(WeatherState state, WeatherSetForecastsAction action)
+        {
+            return state with
+            {
+                Forecasts = action.Forecasts,
+                Loading = false
+            };
+        }
+
+        [ReducerMethod(typeof(WeatherSetInitializedAction))]
+        public static WeatherState OnSetInitialized(WeatherState state)
+        {
+            return state with
+            {
+                Initialized = true
+            };
+        }
+
+        [ReducerMethod(typeof(WeatherLoadForecastAction))]
+        public static WeatherState OnLoadForecast(WeatherState state)
+        {
+            return state with
+            {
+                Loading = true
+            };
+        }        
+    }
+
+    public class WeatherEffects
+    {
+        private readonly WeatherForecastService _weatherForecastService;
+
+        public WeatherEffects(WeatherForecastService weatherForecastService)
+        {
+            _weatherForecastService = weatherForecastService;
+        }
+
+        [EffectMethod(typeof(WeatherLoadForecastAction))]
+        public async Task LoadForecasts(IDispatcher dispatcher)
+        {
+            var forecasts = await _weatherForecastService.GetForecastAsync(DateTime.Now);
+            dispatcher.Dispatch(new WeatherSetForecastsAction(forecasts));
+        }
+    }
+
+    public class WeatherSetInitializedAction { }
+    public class WeatherLoadForecastAction { }
+    public class WeatherSetForecastsAction
+    {
+        public WeatherForecast[] Forecasts { get; }
+
+        public WeatherSetForecastsAction(WeatherForecast[] forecasts)
+        {
+            Forecasts = forecasts;
+        }
+    }
+}
